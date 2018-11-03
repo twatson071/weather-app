e217d64b5ea020958bcbd1e6c52c781034052545
Thomas Watson <twatson071@gmail.com>
2018-11-03 11:37:32 -0700

    moving data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app';
+  searchTerm = 'San Diego';
+  searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+      console.log(result);
+      return result.json();
+    }).then(result => {
+      init(result);
+    })
+  }
+
+}
+let appId = 'c77450d0bc6490eab069d2efed7f8591';
+let units = 'imperial';
+let searchMethod = 'zip'
+function getSearchMethod(searchTerm) {
+  if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+    searchMethod = 'zip';
+  else
+    searchMethod = 'q';
+}
+function init(resultFromServer) {
+  switch (resultFromServer.weather[0].main) {
+    case 'Clear':
+      document.body.style.backgroundImage = 'url("../assets/images/clear.jpg")';
+      break;
+    case 'Clouds':
+      document.body.style.backgroundImage = 'url("../assets/images/clouds.jpg")';
+
+    case 'Rain':
+    case 'Drizzle':
+    case 'Mist':
+      document.body.style.backgroundImage = 'url("../assets/images/rain.jpg")';
+      break;
+
+    case 'Thunderstorm':
+      document.body.style.backgroundImage = 'url("../assets/images/storm.jpg")';
+      break;
+
+    case 'Snow':
+      document.body.style.backgroundImage = 'url("../assets/images/snow.jpg")';
+      break;
+
+    default:
+      break;
+  }
 }
